ca8bb481f3b366d0929ef6bf745f83d7c128327b
ntibbals <ntibbals@gmail.com>
2019-01-13 12:06:20 -0800

    working on second test

--- Lab04_TicTacToe/TicTacTest/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacTest/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace TicTacTest
         [Fact]
         public void TestForWinners()
         {
-            ///Arrange
+            ///Test game will return a winner
             Player p1 = new Player();
             Player p2 = new Player();
             Game play = new Game(p1, p2);
@@ -25,6 +25,18 @@ namespace TicTacTest
             p1.Marker = "X";
             p2.Marker = "O";
             Assert.True(play.CheckForWinner(play.Board));
+        }
+        [Fact]
+        public void TestForSwitch()
+        {
+            ///Test turn will switch
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game play = new Game(p1, p2);
+
+            p1.IsTurn = true;
+            
+            Assert.Equal(p1, play.NextPlayer());
         }
     }
 }
